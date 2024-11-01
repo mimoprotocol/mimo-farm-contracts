4a3b7fa98ba7bf1948c6b917a2b2c27689b0924c
ququzone <xueping.yang@gmail.com>
2024-11-02 01:40:55 +0800

    add smartchef factory

---
+++ src/SmartChefFactory.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20Metadata} from "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+import {SmartChef} from "./SmartChef.sol";
+
+contract SmartChefFactory is Ownable {
+    event NewSmartChefContract(address indexed smartChef);
+
+    function deployPool(
+        IERC20Metadata _stakedToken,
+        IERC20Metadata _rewardToken,
+        uint256 _rewardPerSecond,
+        uint256 _startTimestamp,
+        uint256 _endTimestamp,
+        uint256 _poolLimitPerUser,
+        address _admin
+    ) external onlyOwner returns (address) {
+        SmartChef smartChef = new SmartChef(
+            _stakedToken, _rewardToken, _rewardPerSecond, _startTimestamp, _endTimestamp, _poolLimitPerUser
+        );
+        smartChef.transferOwnership(_admin);
+
+        emit NewSmartChefContract(address(smartChef));
+        return address(smartChef);
+    }
+}
---
+++ test/SmartChefFactory.t.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Test, console} from "forge-std/Test.sol";
+
+import {SmartChef, SmartChefFactory, IERC20Metadata} from "../src/SmartChefFactory.sol";
+import {Token} from "../src/test/Token.sol";
+
+contract SmartChefFactoryTest is Test {
+    SmartChefFactory public factory;
+    address public owner = address(0xa);
+
+    function setUp() public {
+        factory = new SmartChefFactory();
+    }
+
+    function testAdmin() public {
+        Token stakingToken = new Token("Staking", "S");
+        Token rewardToken = new Token("Reward", "R");
+        address smartchef = factory.deployPool(stakingToken, rewardToken, 0.1 ether, 1000, 2000, 0, owner);
+
+        assertEq(SmartChef(smartchef).owner(), owner);
+    }
+}
