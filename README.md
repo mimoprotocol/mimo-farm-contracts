cf08273224101a7bff889c49ac7de48681341692
ququzone <xueping.yang@gmail.com>
2024-11-02 11:38:52 +0800

    complete deploy testnet

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Mimo Farm
+## Mimo Farm contracts
+
+### Testnet
+
+```
+Deployed staking token:  0x1DD6A83B438Ab3ffD7B2812337cd0F028A8c0287
+Deployed reward token:  0x4186a4e95a7906Fbf0EBe54034917A7EcC62Fee9
+Deployed SmartchefFactory:  0x70804bAF4C6FEe2A81EE8b575E6a7595BFE67A35
+```
--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -3,10 +3,32 @@ pragma solidity ^0.8.19;
 
 import {Script, console} from "forge-std/Script.sol";
 
+import {Token} from "../src/test/Token.sol";
+import {SmartChefFactory} from "../src/SmartChefFactory.sol";
+
 contract DeployScript is Script {
-    function setUp() public {}
+    bool public deployStakingToken;
+    bool public deployRewardToken;
+
+    function setUp() public {
+        deployStakingToken = vm.envBool("DEPLOY_STAKING");
+        deployRewardToken = vm.envBool("DEPLOY_REWARD");
+    }
 
     function run() public {
-        vm.broadcast();
+        vm.startBroadcast();
+        if (deployStakingToken) {
+            Token stakingToken = new Token("Test LP", "TLP");
+            console.log("Deployed staking token: ", address(stakingToken));
+        }
+        if (deployRewardToken) {
+            Token rewardToken = new Token("Test Reward", "TRT");
+            console.log("Deployed reward token: ", address(rewardToken));
+        }
+
+        SmartChefFactory factory = new SmartChefFactory();
+        vm.stopBroadcast();
+
+        console.log("Deployed SmartchefFactory: ", address(factory));
     }
 }
